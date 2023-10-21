c5ca3c2eb5218196c5012b6fc27938151276fcb8
Donat <donatborkovskij@gmail.com>
2023-10-21 21:26:05 +0200

    commit

--- src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
@@ -2,6 +2,12 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class TriangleSidesValidator {
     public void validate(double firstSide, double secondSide, double thirdSide) {
-
+        if (firstSide > secondSide && firstSide > thirdSide) {
+            System.out.println((secondSide + thirdSide) > firstSide ? "this is a valid triangle" : "it's not a triangle");
+        } else if (secondSide > firstSide && secondSide > thirdSide) {
+            System.out.println((firstSide + thirdSide) > secondSide ? "this is a valid triangle" : "it's not a triangle");
+        } else {
+            System.out.println((secondSide + firstSide) > thirdSide ? "this is a valid triangle" : "it's not a triangle");
+        }
     }
 }
